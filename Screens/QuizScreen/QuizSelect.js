c7a9629d0b94305b17bc604c5721b477a9eca4e6
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2018-11-29 04:56:05 +0500

    quiz app

--- Screens/QuizScreen/QuizSelect.js
+++ Screens/QuizScreen/QuizSelect.js
@@ -1,12 +1,108 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, View } from 'react-native';
+import { Container, Header, Content, Form, Item, Picker, Text, Icon, Button } from 'native-base';
+
+import { StackActions, NavigationActions } from 'react-navigation'
 
 export default class QuizSelect extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories: ['General Knowledge', 'Entertainment: Books', 'Entertainment: Film', 'Entertainment: Music', 'Entertainment: Musicals & Theatres', 'Entertainment: Television', 'Entertainment: Video Games', 'Entertainment: Board Games', 'Science & Nature', 'Science: Computers', 'Science: Mathematics', 'Mythology', 'Sports', 'Geography', 'History', 'Politics', 'Art', 'Celebrities', 'Animals', 'Vehicles', 'Entertainment: Comics', 'Science: Gadgets', 'Entertainment: Japanese Anime & Manga', 'Entertainment: Cartoon & Animations'],
+            difficulties: ['Easy', 'Medium', 'Hard'],
+
+            category: 0,
+            difficulty: 'Easy',
+        };
+    }
+
+    selectCat(value) {
+        this.setState({
+            category: value
+        });
+    }
+
+    selectDiff(value) {
+        this.setState({
+            difficulty: value
+        });
+    }
+
+    startQuiz() {
+        let { category, difficulty } = this.state;
+        category = category + 9;
+        difficulty = difficulty.toLowerCase()
+        console.log(category, difficulty)
+
+        const resetAction = StackActions.reset({
+            index: 0,
+            actions: [
+                NavigationActions.navigate({
+                    routeName: 'QuizStart',
+                    params: {
+                        quizParams: {
+                            category,
+                            difficulty,
+                        }
+                    }
+                }),
+            ],
+        });
+        this.props.navigation.dispatch(resetAction);
+
+    }
+
     render() {
+        const { categories, difficulties } = this.state;
+        console.log(this.state.category, '|', this.state.difficulty)
         return (
-            <View style={{ flex: 1 }}>
-                <Text>QuizSelect</Text>
-            </View>
+            <Container>
+                <Content padder>
+                    <Form>
+                        <Item picker>
+                            <Picker
+                                mode="dropdown"
+                                iosIcon={<Icon name="ios-arrow-down-outline" />}
+                                // style={{ width: '100%' }}
+                                placeholder="Category"
+                                placeholderStyle={{ color: "#bfc6ea" }}
+                                placeholderIconColor="#007aff"
+                                selectedValue={this.state.category}
+                                onValueChange={this.selectCat.bind(this)}
+                            >
+                                {categories.map((value, index) => {
+                                    return <Picker.Item label={value} value={index} key={index} />
+                                })}
+
+                            </Picker>
+                        </Item>
+
+                        <Item picker>
+                            <Picker
+                                mode="dropdown"
+                                iosIcon={<Icon name="ios-arrow-down-outline" />}
+                                // style={{ width: '100%' }}
+                                placeholder="Difficulty Level"
+                                placeholderStyle={{ color: "#bfc6ea" }}
+                                placeholderIconColor="#007aff"
+                                selectedValue={this.state.difficulty}
+                                onValueChange={this.selectDiff.bind(this)}
+                            >
+                                {difficulties.map((value, index) => {
+                                    return <Picker.Item label={value} value={value} key={index} />
+                                })}
+
+                            </Picker>
+                        </Item>
+                    </Form>
+                    <Button block primary
+                        onPress={() => this.startQuiz()}
+                    >
+                        <Text>Start Quiz</Text>
+                    </Button>
+                </Content>
+            </Container>
         );
     }
 }
--- Screens/QuizScreen/QuizStart.js
+++ Screens/QuizScreen/QuizStart.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { StackActions, NavigationActions } from 'react-navigation'
+
 export default class QuizStart extends React.Component {
+
+    constructor(props) {
+        super(props);
+  
+        this.state = {
+  
+        };
+    }
+
+getQuiz(){
+    
+    const { category, difficulty } = this.props.navigation.state.params.quizParams;
+    console.log(this.props.navigation.state.params.quizParams)
+    
+    fetch(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=multiple`)
+            .then((res) => {
+                // console.log("api", res.json())
+                return res.json()
+            }).then(result => {  
+                console.log("quiz result", result)
+                // this.setState({
+                //     searchLocations
+                // })
+            })
+}
+
+
+componentDidMount(){
+    this.getQuiz();
+}
+
     render() {
+        // console.log(this.props.navigation.state.params.quizParams)
         return (
             <View style={{ flex: 1 }}>
                 <Text>QuizStart</Text>
