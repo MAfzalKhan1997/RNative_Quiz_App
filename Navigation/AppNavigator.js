89f9fce2615226bdff37ec35dbe3dc31d4803fd9
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2018-12-05 23:09:46 +0500

    quiz-app

--- Navigation/AppNavigator.js
+++ Navigation/AppNavigator.js
@@ -1,25 +1,65 @@
-import HomeScreen from  '../Screens/HomeScreen/HomeScreen'
-import QuizSelect from  '../Screens/QuizScreen/QuizSelect'
-import QuizStart from  '../Screens/QuizScreen/QuizStart'
-import QuizResult from  '../Screens/QuizScreen/QuizResult' 
+import HomeScreen from '../Screens/HomeScreen/HomeScreen'
+import QuizSelect from '../Screens/QuizScreen/QuizSelect'
+import QuizStart from '../Screens/QuizScreen/QuizStart'
+import QuizResult from '../Screens/QuizScreen/QuizResult'
 
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 const StackNavigator = createStackNavigator({
     Home: {
-        screen: HomeScreen
+        screen: HomeScreen,
+        navigationOptions: () => ({
+            title: 'Home',
+            headerStyle: {
+                backgroundColor: '#3f51b5',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+        })
     },
     QuizSelect: {
-        screen: QuizSelect
+        screen: QuizSelect,
+        navigationOptions: () => ({
+            title: 'Select',
+            headerStyle: {
+                backgroundColor: '#3f51b5',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+        })
     },
     QuizStart: {
-        screen: QuizStart
+        screen: QuizStart,
+        navigationOptions: () => ({
+            title: 'Quiz',
+            headerStyle: {
+                backgroundColor: '#3f51b5',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+        })
     },
     QuizResult: {
-        screen: QuizResult
+        screen: QuizResult,
+        navigationOptions: () => ({
+            title: 'Result',
+            headerStyle: {
+                backgroundColor: '#3f51b5',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+        })
     },
 })
-  
+
 const Navigator = createAppContainer(StackNavigator)
 
 export default Navigator
