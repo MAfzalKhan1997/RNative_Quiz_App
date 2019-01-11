fa978bd04a3f62490e02136ad5c319b16dc11167
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2019-01-11 22:50:10 +0500

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigator from './Navigation/AppNavigator' 
 
-
 export default class App extends React.Component {
   render() {
     return (
